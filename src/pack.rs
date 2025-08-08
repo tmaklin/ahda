cedaa29d415e6d1768e9b356335c2394f18a2c7e
tmaklin <tommi@maklin.fi>
2025-08-08 14:37:43 +0200

    Pack in a format that allows inferring read_id

--- src/pack.rs
+++ src/pack.rs
@@ -16,14 +16,7 @@ use crate::headers::block::BlockHeader;
 use crate::headers::block::encode_block_header;
 use crate::headers::block::encode_block_flags;
 
-use std::io::Write;
-
 use bitmagic::BVector;
-
-use dsi_bitstream::impls::MemWordWriterVec;
-use dsi_bitstream::impls::BufBitWriter;
-use dsi_bitstream::traits::BE;
-use dsi_bitstream::codes::MinimalBinaryWrite;
 
 type E = Box<dyn std::error::Error>;
 
@@ -38,43 +31,22 @@ impl std::fmt::Display for EncodeError {
 
 impl std::error::Error for EncodeError {}
 
-fn minimal_binary_encode(
-    ints: &[u64],
-) -> Result<(Vec<u64>, u64), E> {
-    let param: u64 = *ints.iter().max().ok_or(EncodeError)?;
-    assert!(param < u64::MAX - 2);
-    let param = if param > u64::MAX - 2 { u64::MAX } else { param + 2 };
-
-    let word_write = MemWordWriterVec::new(Vec::<u64>::new());
-    let mut writer = BufBitWriter::<BE, _>::new(word_write);
-
-    for n in ints {
-        writer.write_minimal_binary(*n + 1, param)?;
-    }
-
-    writer.flush()?;
-
-    Ok((writer.into_inner()?.into_inner(), param))
-}
-
 /// Converts [PseudoAln] records to BitMagic bitvectors
 pub fn convert_to_bitmagic(
     records: &[PseudoAln],
-) -> Result<(BVector, usize), E> {
-    let offset = records.iter().map(|record| record.read_id).min().unwrap_or(0) as usize;
-
+) -> Result<BVector, E> {
     let mut bits: BVector = BVector::new();
-
     let n_targets = records[0].ones.len();
 
-    records.iter().enumerate().for_each(|(read_idx, record)| {
+    records.iter().for_each(|record| {
+        let read_idx = record.read_id as usize;
         record.ones.iter().enumerate().for_each(|(bit_idx, is_set)| {
             let index = read_idx*n_targets + bit_idx;
             bits.set(index, *is_set);
         });
     });
 
-    Ok((bits, offset))
+    Ok(bits)
 }
 
 pub fn serialize_bvector(
@@ -88,38 +60,23 @@ pub fn serialize_bvector(
 pub fn pack(
     records: &[PseudoAln],
 ) -> Result<Vec<u8>, E> {
-    let ids = records.iter().map(|record| {
-        record.read_id as u64
-    }).collect::<Vec<u64>>();
-    let encoded_1 = minimal_binary_encode(&ids)?;
+    let alignments = convert_to_bitmagic(records)?;
 
-    let (alignments, offset) = convert_to_bitmagic(records)?;
-
-    // TODO Error if offset * n_targets exceeds 32-bit BitMagic capacity
     let mut encoded_2 = serialize_bvector(&alignments)?;
-
-    let mut data: Vec<u8> = encoded_1.0.iter().flat_map(|record| {
-        let bytes: Vec<u8> = record.to_ne_bytes()[0..8].to_vec();
-        let mut arr: [u8; 8] = [0; 8];
-        arr[0..8].copy_from_slice(&bytes);
-        arr
-    }).collect();
 
     let mut block_flags: Vec<u8> = encode_block_flags(&Vec::new())?;
     let header = BlockHeader{ flags_len: block_flags.len() as u32,
                               num_records: records.len() as u32,
                               alignments_u64: encoded_2.len() as u32,
-                              ids_u64: data.len() as u32,
-                              alignments_param: offset as u64,
-                              ids_param: encoded_1.1,
+                              ids_u64: 0,
+                              alignments_param: 0,
+                              ids_param: 0,
     };
 
     let mut block: Vec<u8> = encode_block_header(&header)?;
     assert_eq!(block.len(), 32);
     block.append(&mut block_flags);
     assert_eq!(block.len(), 32 + header.flags_len as usize);
-    block.append(&mut data);
-    assert_eq!(block.len(), 32 + header.flags_len as usize + header.ids_u64 as usize);
     block.append(&mut encoded_2);
     assert_eq!(block.len(), 32 + header.flags_len as usize + header.ids_u64 as usize + header.alignments_u64 as usize);
 
--- src/unpack.rs
+++ src/unpack.rs
@@ -18,26 +18,7 @@ use std::io::Read;
 
 use bitmagic::BVector;
 
-use dsi_bitstream::traits::BE;
-use dsi_bitstream::prelude::MemWordReader;
-use dsi_bitstream::prelude::BufBitReader;
-use dsi_bitstream::codes::MinimalBinaryRead;
-
 type E = Box<dyn std::error::Error>;
-
-fn minimal_binary_decode(
-    encoded: &[u64],
-    n_records: usize,
-    param: u64,
-) -> Result<Vec<u64>, E> {
-    let mut reader = BufBitReader::<BE, _>::new(MemWordReader::new(encoded));
-
-    let mut decoded: Vec<u64> = vec![0; n_records];
-    for item in decoded.iter_mut().take(n_records) {
-        *item = reader.read_minimal_binary(param)? - 1;
-    }
-    Ok(decoded)
-}
 
 pub fn decode_bitvec(
     bytes: &[u8],
@@ -54,43 +35,22 @@ pub fn decode_bitvec(
     Ok(res)
 }
 
-pub fn decode_ids(
-    bytes: &[u8],
-    num_ids: usize,
-    param: u64,
-) -> Result<Vec<u64>, E> {
-    let ids_u64: Vec<u64> = bytes.chunks(8).map(|chunk| {
-        let mut arr: [u8; 8] = [0; 8];
-        arr[0..chunk.len()].copy_from_slice(chunk);
-        u64::from_ne_bytes(arr)
-    }).collect();
-
-    let ids = minimal_binary_decode(&ids_u64, num_ids, param)?;
-
-    Ok(ids)
-}
 pub fn unpack<R: Read>(
     block_header: &BlockHeader,
     n_targets: usize,
     conn: &mut R,
 ) -> Result<Vec<PseudoAln>, E> {
-    let mut id_bytes: Vec<u8> = vec![0; block_header.ids_u64 as usize];
-    conn.read_exact(&mut id_bytes)?;
-
     let mut aln_bytes: Vec<u8> = vec![0; block_header.alignments_u64 as usize];
     conn.read_exact(&mut aln_bytes)?;
 
     let aln_bits = decode_bitvec(&aln_bytes, block_header.num_records as usize, n_targets)?;
 
-    let ids = decode_ids(&id_bytes, block_header.num_records as usize, block_header.ids_param)?;
-
-    assert_eq!(ids.len(), block_header.num_records as usize);
     assert_eq!(aln_bits.len() / n_targets, block_header.num_records as usize);
 
-    let alns = ids.iter().enumerate().map(|(idx, id)| {
+    let alns = aln_bits.chunks(n_targets).enumerate().map(|(idx, _)| {
         let start: usize = idx  * n_targets;
         let end: usize = (idx + 1) * n_targets;
-        PseudoAln{ read_id: *id as u32, ones: aln_bits[start..end].to_vec() }
+        PseudoAln{ read_id: idx as u32, ones: aln_bits[start..end].to_vec() }
     }).collect();
 
     Ok(alns)
