8e4ef0530e9764221b632c8f1624cbf30abdd08a
tmaklin <tommi@maklin.fi>
2025-08-11 13:58:40 +0200

    Add guess_format (does not fill n_targets)

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -33,22 +33,29 @@ use std::io::Read;
 pub struct Parser<'a, R: Read> {
     // conn: R,
     reader: BufReader<&'a mut R>,
-    format: Format,
+    buf: Cursor<Vec<u8>>,
+    pub format: Format,
 }
 
 impl<'a, R: Read> Parser<'a, R> {
     pub fn new(
         conn: &'a mut R,
     ) -> Self {
-        // TODO try to infer the format
-        Self { reader: BufReader::new(conn), format: Format::default() }
+        let mut reader = BufReader::new(conn);
+        let mut buf = Cursor::new(Vec::<u8>::new());
+
+        reader.read_until(b'\n', buf.get_mut()).unwrap();
+
+        let format = guess_format(buf.get_ref()).unwrap();
+
+        Self { reader, buf, format }
     }
 
     pub fn new_with_format(
         conn: &'a mut R,
         format: &Format,
     ) -> Self {
-        Self { reader: BufReader::new(conn), format: format.clone() }
+        Self { reader: BufReader::new(conn), format: format.clone(), buf: Cursor::new(Vec::new()) }
     }
 }
 
@@ -56,7 +63,24 @@ impl<R: Read> Parser<'_, R> {
     pub fn next(
         &mut self,
     ) -> Option<PseudoAln> {
+
+        // TODO this is a dumb implementation, fix
+
         let mut line = Cursor::new(Vec::<u8>::new());
+        if !self.buf.get_ref().is_empty() {
+            line = self.buf.clone();
+            if line.get_mut().contains(&b'\n') {
+                line.get_mut().pop();
+            }
+            let res = match self.format {
+                Format::Themisto{ n_targets: num_targets } => read_themisto(num_targets, &mut line).unwrap(),
+                Format::Fulgor{ n_targets: num_targets } => read_fulgor(num_targets, &mut line).unwrap(),
+                Format::Bifrost => read_bifrost(&mut line).unwrap(),
+            };
+            self.buf.get_mut().clear();
+            return Some(res)
+        }
+
         if self.reader.read_until(b'\n', line.get_mut()).is_ok() {
             if line.get_mut().is_empty() {
                 return None
@@ -81,6 +105,34 @@ impl<R: Read> Parser<'_, R> {
     }
 }
 
+pub fn guess_format(
+    bytes: &[u8],
+) -> Option<Format> {
+    let not_themisto: bool = bytes.contains(&b'\t');
+    if !not_themisto {
+        return Some(Format::Themisto{ n_targets: 0 })
+    }
+
+    let line = bytes.iter().map(|x| *x as char).collect::<String>();
+    let mut records = line.split('\t');
+
+    let bifrost: bool = records.next()? == "query_name";
+    if bifrost {
+        return Some(Format::Bifrost)
+    }
+
+    let mut next = records.next()?;
+    if records.next().is_none() {
+        next = &next[0..(next.len() - 1)];
+    }
+
+    let fulgor: bool = next.parse::<u32>().is_ok();
+    if fulgor {
+        return Some(Format::Fulgor{ n_targets: 0 })
+    }
+
+    None
+}
 // impl<'a, R: Read> Iterator for &'a Parser<'a, R> {
 //     type Item = PseudoAln;
 
