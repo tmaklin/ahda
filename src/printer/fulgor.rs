7cc0e391ef831ce8db5a4197b0102cec6c3b6cec
tmaklin <tommi@maklin.fi>
2025-08-13 10:03:03 +0200

    Error if ones or query_id is None, add tests.

--- src/printer/fulgor.rs
+++ src/printer/fulgor.rs
@@ -17,10 +17,23 @@ use crate::PseudoAln;
 
 type E = Box<dyn std::error::Error>;
 
+#[derive(Debug, Clone)]
+pub struct FulgorPrinterError;
+
+impl std::fmt::Display for FulgorPrinterError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "invalid input to encode")
+    }
+}
+
+impl std::error::Error for FulgorPrinterError {}
+
 /// Format a single pseudoalignment in Fulgor format
 ///
 /// Writes bytes containing the formatted line containing the contents of
 /// `aln` to `conn`.
+///
+/// Terminates with a [FulgorPrinterError] if [PseudoAln::query_id] or [PseudoAln::ones] is None.
 ///
 pub fn format_fulgor_line<W: Write>(
     aln: &PseudoAln,
@@ -29,14 +42,14 @@ pub fn format_fulgor_line<W: Write>(
     let separator: char = '\t';
     let mut formatted: String = String::new();
 
-    // TODO error if query_name is None
+    if aln.ones.is_none() || aln.query_id.is_none() {
+        return Err(Box::new(FulgorPrinterError{}))
+    }
+
     formatted += &aln.query_name.clone().unwrap().to_string();
     formatted += &separator.to_string();
-    let aln_count_pos = formatted.len(); // TODO update count
-    formatted += &0.to_string();
-    formatted += &separator.to_string();
+    formatted += &aln.ones.as_ref().unwrap().len().to_string();
 
-    // TODO error if ones is None
     aln.ones.as_ref().unwrap().iter().for_each(|idx| {
         formatted += &separator.to_string();
         formatted += &idx.to_string();
@@ -51,18 +64,93 @@ pub fn format_fulgor_line<W: Write>(
 #[cfg(test)]
 mod tests {
 
-    // #[test]
-    // fn format_themisto_line() {
-    //     use std::io::Cursor;
-    //     use crate::PseudoAln;
-    //     use super::format_themisto_line;
+    #[test]
+    fn format_fulgor_line_1st_aligned() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_fulgor_line;
 
-    //     let data = PseudoAln{ones_names: None,  query_id: Some(128), ones: vec![true, false, false, true, false, false, false, true, false, false, false, true], ..Default::default()};
-    //     let expected: Vec<u8> = vec![49, 50, 56, 32, 48, 32, 51, 32, 55, 32, 49, 49, 10];
+        let data = PseudoAln{ones_names: None,  query_id: Some(1262953), ones: Some(vec![0]), query_name: Some("ERR4035126.1262954".to_string()) };
 
-    //     let mut got: Vec<u8> = Vec::new();
-    //     format_themisto_line(&data, &mut got).unwrap();
+        let expected: Vec<u8> = b"ERR4035126.1262954\t1\t0\n".to_vec();
 
-    //     assert_eq!(got, expected);
-    // }
+        let mut got: Vec<u8> = Vec::new();
+        format_fulgor_line(&data, &mut got).unwrap();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn format_fulgor_line_2nd_aligned() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_fulgor_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: Some(1262953), ones: Some(vec![1]), query_name: Some("ERR4035126.1262954".to_string()) };
+
+        let expected: Vec<u8> = b"ERR4035126.1262954\t1\t1\n".to_vec();
+
+        let mut got: Vec<u8> = Vec::new();
+        format_fulgor_line(&data, &mut got).unwrap();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn format_fulgor_line_two_alignments() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_fulgor_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: Some(1262953), ones: Some(vec![0, 1]), query_name: Some("ERR4035126.1262954".to_string()) };
+
+        let expected: Vec<u8> = b"ERR4035126.1262954\t2\t0\t1\n".to_vec();
+
+        let mut got: Vec<u8> = Vec::new();
+        format_fulgor_line(&data, &mut got).unwrap();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn format_fulgor_line_no_alignments() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_fulgor_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: Some(1262953), ones: Some(vec![]), query_name: Some("ERR4035126.1262954".to_string()) };
+
+        let expected: Vec<u8> = b"ERR4035126.1262954\t0\n".to_vec();
+
+        let mut got: Vec<u8> = Vec::new();
+        format_fulgor_line(&data, &mut got).unwrap();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn error_if_no_query_id() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_fulgor_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: None, ones: Some(vec![0, 3, 7, 11]), ..Default::default()};
+
+        let got = format_fulgor_line(&data, &mut Vec::new());
+
+        assert!(!got.is_ok());
+    }
+
+    #[test]
+    fn error_if_no_ones() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_fulgor_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: Some(128), ones: None, ..Default::default()};
+
+        let got = format_fulgor_line(&data, &mut Vec::new());
+
+        assert!(!got.is_ok());
+    }
 }
--- src/printer/themisto.rs
+++ src/printer/themisto.rs
@@ -17,10 +17,24 @@ use crate::PseudoAln;
 
 type E = Box<dyn std::error::Error>;
 
+#[derive(Debug, Clone)]
+pub struct ThemistoPrinterError;
+
+impl std::fmt::Display for ThemistoPrinterError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "invalid input to encode")
+    }
+}
+
+impl std::error::Error for ThemistoPrinterError {}
+
 /// Format a single pseudoalignment in Themisto format
 ///
 /// Writes bytes containing the formatted line containing the contents of
 /// `aln` to `conn`.
+///
+/// Terminates with a [ThemistoPrinterError] if [PseudoAln::query_id] or
+/// [PseudoAln::ones] is None.
 ///
 pub fn format_themisto_line<W: Write>(
     aln: &PseudoAln,
@@ -29,10 +43,12 @@ pub fn format_themisto_line<W: Write>(
     let separator: char = ' ';
     let mut formatted: String = String::new();
 
-    // TODO error if query_id is None
+    if aln.ones.is_none() || aln.query_id.is_none() {
+        return Err(Box::new(ThemistoPrinterError{}))
+    }
+
     formatted += &aln.query_id.unwrap().to_string();
 
-    // TODO error if ones is None
     aln.ones.as_ref().unwrap().iter().for_each(|idx| {
         formatted += &separator.to_string();
         formatted += &idx.to_string();
@@ -48,17 +64,72 @@ pub fn format_themisto_line<W: Write>(
 mod tests {
 
     #[test]
-    fn format_themisto_line() {
+    fn format_themisto_line_single_alignment() {
         use std::io::Cursor;
         use crate::PseudoAln;
         use super::format_themisto_line;
 
-        let data = PseudoAln{ones_names: None,  query_id: Some(128), ones: Some(vec![0, 3, 7, 11]), ..Default::default()};
-        let expected: Vec<u8> = vec![49, 50, 56, 32, 48, 32, 51, 32, 55, 32, 49, 49, 10];
+        let data = PseudoAln{ones_names: None,  query_id: Some(128), ones: Some(vec![0]), ..Default::default()};
+        let expected: Vec<u8> = b"128 0\n".to_vec();
 
         let mut got: Vec<u8> = Vec::new();
         format_themisto_line(&data, &mut got).unwrap();
 
         assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn format_themisto_line_many_alignments() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_themisto_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: Some(128), ones: Some(vec![0, 3, 7, 11]), ..Default::default()};
+        let expected: Vec<u8> = b"128 0 3 7 11\n".to_vec();
+
+        let mut got: Vec<u8> = Vec::new();
+        format_themisto_line(&data, &mut got).unwrap();
+
+        assert_eq!(got, expected);
+    }
+
+    fn format_themisto_line_no_alignments() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_themisto_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: Some(128), ones: Some(vec![]), ..Default::default()};
+        let expected: Vec<u8> = b"128\n".to_vec();
+
+        let mut got: Vec<u8> = Vec::new();
+        format_themisto_line(&data, &mut got).unwrap();
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn error_if_no_query_id() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_themisto_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: None, ones: Some(vec![0, 3, 7, 11]), ..Default::default()};
+
+        let got = format_themisto_line(&data, &mut Vec::new());
+
+        assert!(!got.is_ok());
+    }
+
+    #[test]
+    fn error_if_no_ones() {
+        use std::io::Cursor;
+        use crate::PseudoAln;
+        use super::format_themisto_line;
+
+        let data = PseudoAln{ones_names: None,  query_id: Some(128), ones: None, ..Default::default()};
+
+        let got = format_themisto_line(&data, &mut Vec::new());
+
+        assert!(!got.is_ok());
     }
 }
