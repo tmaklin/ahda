82455fdfcabc6d1a4ec53ea4d7d650020435968a
tmaklin <tommi@maklin.fi>
2025-08-25 12:17:22 +0200

    Implement formatting SAM lines from PseudoAln

--- src/printer/sam.rs
+++ src/printer/sam.rs
@@ -11,12 +11,12 @@
 // the MIT license, <LICENSE-MIT> or <http://opensource.org/licenses/MIT>,
 // at your option.
 //
-use std::io::Write;
 
 use bstr::BString;
 use indexmap::map::IndexMap;
 use noodles_sam::{
     self as sam,
+    alignment::io::Write,
     header::record::value::{map::ReadGroup, map::ReferenceSequence, Map},
 };
 
@@ -45,22 +45,28 @@ impl std::error::Error for SamPrinterError {}
 /// Terminates with a [SamPrinterError] if [PseudoAln::query_id] or
 /// [PseudoAln::ones] is None.
 ///
-pub fn format_sam_line<W: Write>(
+pub fn format_sam_line<W: std::io::Write>(
     aln: &PseudoAln,
+    header: &sam::Header,
     conn: &mut W,
 ) -> Result<(), E> {
     if aln.ones.is_none() || aln.query_name.is_none() {
         return Err(Box::new(SamPrinterError{}))
     }
 
+    let mut writer = noodles_sam::io::Writer::new(Vec::new());
+
+    // TODO Error if query_name or ones is None
+
     for target_id in aln.ones.as_ref().unwrap() {
         let record = sam::alignment::RecordBuf::builder()
             .set_name(aln.query_name.clone().unwrap())
             .set_reference_sequence_id(*target_id as usize)
             .build();
+        writer.write_alignment_record(header, &record)?;
     }
-    // See https://docs.rs/noodles-sam/latest/noodles_sam/io/writer/struct.Writer.html
-    // for more details on how to continue from here
+    conn.write_all(writer.get_ref())?;
+
     Ok(())
 }
 
@@ -94,17 +100,29 @@ mod tests {
 
     #[test]
     fn format_sam_line_aligned() {
-        use crate::PseudoAln;
+        use crate::headers::file::FileHeader;
+        use crate::headers::file::FileFlags;
+        use super::format_sam_header;
         use super::format_sam_line;
+        use crate::PseudoAln;
 
-        let data = PseudoAln{ones_names: Some(vec!["OZ038621.1".to_string()]), query_id: None, ones: Some(vec![]), query_name: Some("ERR4035126.1".to_string()) };
+        // Build header
+        let fheader = FileHeader { n_targets: 2, ..Default::default() };
+        let fflags = FileFlags { target_names: vec!["chr.fasta".to_string(), "plasmid.fasta".to_string()], query_name: "test.fastq".to_string() };
+        let mut expected: Vec<u8> = b"@HD\tVN:1.6\n".to_vec();
+        expected.append(&mut b"@SQ\tSN:chr.fasta\tLN:1\n".to_vec());
+        expected.append(&mut b"@SQ\tSN:plasmid.fasta\tLN:1\n".to_vec());
+        expected.append(&mut b"@RG\tID:test.fastq\n".to_vec());
+        let header = format_sam_header(&fheader, &fflags).unwrap();
 
-        let expected: Vec<u8> =b"ERR4035126.1\t16\tOZ038621.1\t4541508\t60\t151M\t*\t0\t0\tAGTATTTAGTGACCTAAGTCAATAAAATTTTAATTTACTCACGGCAGGTAACCAGTTCAGAAGCTGCTATCAGACACTCTTTTTTTAATCCACACAGAGACATATTGCCCGTTGCAGTCAGAATGAAAAGCTGAAAATCACTTACTAAGGC FJ<<JJFJAA<-JFAJFAF<JFFJJJJJJJFJFJJA<A<AJJAAAFFJJJJFJJFJFJAJJ7JJJJJFJJJJJFFJFFJFJJJJJJFJ7FFJAJJJJJJJJFJJFJJFJFJJJJFJJFJJJJJJJJJFFJJJJJJJJJJJJJFJJJFFAAA\tNM:i:0\tMD:Z:151\tAS:i:151\tXS:i:0\n".to_vec();
+        let data = PseudoAln{ones_names: Some(vec!["OZ038621.1".to_string()]), query_id: None, ones: Some(vec![1]), query_name: Some("ERR4035126.1".to_string()) };
+
+        let expected: Vec<u8> =b"ERR4035126.1\t4\tplasmid.fasta\t0\t255\t*\t*\t0\t0\t*\t*\n".to_vec();
 
         let mut got: Vec<u8> = Vec::new();
-        format_sam_line(&data, &mut got).unwrap();
+        format_sam_line(&data, &header, &mut got).unwrap();
 
-        assert_eq!(got, expected)
+        assert_eq!(got.iter().map(|x| *x as char).collect::<String>(), expected.iter().map(|x| *x as char).collect::<String>())
     }
 
     #[test]
