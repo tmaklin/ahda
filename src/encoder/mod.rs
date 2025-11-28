8778ea836d26736ed70e3790bb7332c9127ae107
tmaklin <tommi@maklin.fi>
2025-11-28 14:35:27 +0100

    Make Encoder assume all PseudoAln fields are set

--- src/encoder/mod.rs
+++ src/encoder/mod.rs
@@ -17,14 +17,9 @@ use crate::headers::file::FileFlags;
 use crate::headers::file::encode_file_header;
 use crate::headers::file::encode_file_flags;
 
-use std::collections::HashMap;
-
-// TODO records should be anything that implements `next`
 pub struct Encoder<'a, I: Iterator> where I: Iterator<Item=PseudoAln> {
     // Inputs
     records: &'a mut I,
-    query_to_pos: HashMap<String, usize>,
-    pos_to_query: HashMap<usize, String>,
 
     // These are given as construtor parameters
     header: Option<FileHeader>,
@@ -44,17 +39,9 @@ impl<'a, I: Iterator> Encoder<'a, I> where I: Iterator<Item=PseudoAln> {
         queries: &[String],
         sample_name: &str,
     ) -> Self {
-
-        let mut query_to_pos: HashMap<String, usize> = HashMap::new();
-        let mut pos_to_query: HashMap<usize, String> = HashMap::new();
-        queries.iter().enumerate().for_each(|(idx, query)| {
-            query_to_pos.insert(query.clone(), idx);
-            pos_to_query.insert(idx, query.clone());
-        });
-
         let flags = FileFlags{ target_names: targets.to_vec(), query_name: sample_name.to_string() };
         let flags_bytes = crate::headers::file::encode_file_flags(&flags).unwrap();
-        let header = FileHeader{ n_targets: targets.len() as u32, n_queries: query_to_pos.len() as u32, flags_len: flags_bytes.len() as u32, format: 1_u16, ph2: 0, ph3: 0, ph4: 0 };
+        let header = FileHeader{ n_targets: targets.len() as u32, n_queries: queries.len() as u32, flags_len: flags_bytes.len() as u32, format: 1_u16, ph2: 0, ph3: 0, ph4: 0 };
 
         // Adjust block size to fit within 32-bit address space
         let block_size = ((u32::MAX as u64) / header.n_targets as u64).min(65537_u64) as usize;
@@ -62,7 +49,7 @@ impl<'a, I: Iterator> Encoder<'a, I> where I: Iterator<Item=PseudoAln> {
         let block_size = block_size - 1;
 
         Encoder{
-            records, query_to_pos, pos_to_query,
+            records,
             header: Some(header), flags: Some(flags),
             block: Some(Vec::new()), block_index: 0_usize, block_size, blocks_written: 0_usize,
         }
@@ -74,15 +61,8 @@ impl<I: Iterator> Encoder<'_, I> where I: Iterator<Item=PseudoAln> {
         &mut self,
     ) -> Option<Vec<u8>> {
         let mut block_records: Vec<PseudoAln> = Vec::with_capacity(self.block_size);
-        for mut record in self.records.by_ref() {
-            record.query_id = if record.query_id.is_some() { record.query_id } else { Some(*self.query_to_pos.get(&record.query_name.clone().unwrap()).unwrap() as u32) };
-            record.query_name = if record.query_name.is_some() { record.query_name } else { Some(self.pos_to_query.get(&(record.query_id.unwrap() as usize)).unwrap().clone()) };
-
-            record.ones_names = if record.ones_names.is_some() { record.ones_names } else {
-                Some(record.ones.as_ref().unwrap().iter().map(|target_idx| {
-                        self.flags.as_ref().unwrap().target_names[*target_idx as usize].clone()
-                }).collect::<Vec<String>>())};
-
+        for record in self.records.by_ref() {
+            // TODO Check that all fields are set?
             block_records.push(record);
             if block_records.len() == self.block_size {
                 break;
