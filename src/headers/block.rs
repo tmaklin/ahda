fec64a67f87aae17839e56bce87d49e613bdd85b
tmaklin <tommi@maklin.fi>
2025-12-05 11:39:23 +0100

    Derive PartialEq for BlockFlags

--- src/headers/block.rs
+++ src/headers/block.rs
@@ -43,7 +43,7 @@ pub struct BlockHeader {
 ///
 /// Contents may differ between implementations.
 ///
-#[derive(Encode, Clone, Decode, Debug)]
+#[derive(Clone, Decode, Debug, Encode, PartialEq)]
 pub struct BlockFlags {
     /// Names of query records
     pub queries: Vec<String>,
