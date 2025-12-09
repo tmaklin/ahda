61093a933e58997fa851f19092ae50964f2ce8fa
tmaklin <tommi@maklin.fi>
2025-12-09 10:17:28 +0100

    Add tests for different cases

--- src/decoder/bitmap.rs
+++ src/decoder/bitmap.rs
@@ -96,7 +96,7 @@ impl<I: Iterator> Iterator for BitmapDecoder<'_, I> where I: Iterator<Item=u32>{
 mod tests {
 
     #[test]
-    fn next() {
+    fn next_ends_with_one() {
         use super::BitmapDecoder;
         use crate::PseudoAln;
         use crate::headers::file::build_header_and_flags;
@@ -109,7 +109,6 @@ mod tests {
             PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(1), ones: Some(vec![0]), query_name: Some("ERR4035126.2".to_string()) },
             PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(0), ones: Some(vec![0]), query_name: Some("ERR4035126.1".to_string()) },
             PseudoAln{ones_names: Some(vec!["chr.fasta".to_string(), "plasmid.fasta".to_string()]),  query_id: Some(2), ones: Some(vec![0, 1]), query_name: Some("ERR4035126.651903".to_string()) },
-            PseudoAln{ones_names: Some(vec![]),  query_id: Some(4), ones: Some(vec![]), query_name: Some("ERR4035126.16".to_string()) },
             PseudoAln{ones_names: Some(vec!["plasmid.fasta".to_string()]),  query_id: Some(3), ones: Some(vec![1]), query_name: Some("ERR4035126.7543".to_string()) },
         ];
         expected.sort_by_key(|x| *x.query_id.as_ref().unwrap());
@@ -119,6 +118,130 @@ mod tests {
         data.insert(2);
         data.insert(4);
         data.insert(5);
+        data.insert(7);
+
+        let targets = vec!["chr.fasta".to_string(), "plasmid.fasta".to_string()];
+        let queries = vec!["ERR4035126.1".to_string(), "ERR4035126.2".to_string(), "ERR4035126.651903".to_string(), "ERR4035126.7543".to_string()];
+        let query_ids = vec![0, 1, 2, 3];
+        let block_flags = BlockFlags { queries: queries.clone(), query_ids };
+        let block_header = BlockHeader { num_records: 0, deflated_len: 0, block_len: 0, flags_len: 0, start_idx: 0, placeholder2: 0, placeholder3: 0 };
+        let (header, flags) = build_header_and_flags(&targets, &queries, &"ERR4035126".to_string()).unwrap();
+
+        let mut tmp = data.iter();
+        let mut bdecoder = BitmapDecoder::new(&mut tmp, header, flags, block_header, block_flags);
+
+        let mut got: Vec<PseudoAln> = Vec::with_capacity(expected.len());
+        for record in bdecoder.by_ref() {
+            got.push(record);
+        }
+        got.sort_by_key(|x| *x.query_id.as_ref().unwrap());
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn next_ends_with_ones() {
+        use super::BitmapDecoder;
+        use crate::PseudoAln;
+        use crate::headers::file::build_header_and_flags;
+        use crate::headers::block::BlockFlags;
+        use crate::headers::block::BlockHeader;
+
+        use roaring::RoaringBitmap;
+
+        let mut expected = vec![
+            PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(1), ones: Some(vec![0]), query_name: Some("ERR4035126.2".to_string()) },
+            PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(0), ones: Some(vec![0]), query_name: Some("ERR4035126.1".to_string()) },
+            PseudoAln{ones_names: Some(vec!["chr.fasta".to_string(), "plasmid.fasta".to_string()]),  query_id: Some(2), ones: Some(vec![0, 1]), query_name: Some("ERR4035126.651903".to_string()) },
+        ];
+        expected.sort_by_key(|x| *x.query_id.as_ref().unwrap());
+
+        let mut data = RoaringBitmap::new();
+        data.insert(0);
+        data.insert(2);
+        data.insert(4);
+        data.insert(5);
+
+        let targets = vec!["chr.fasta".to_string(), "plasmid.fasta".to_string()];
+        let queries = vec!["ERR4035126.1".to_string(), "ERR4035126.2".to_string(), "ERR4035126.651903".to_string(), "ERR4035126.7543".to_string(), "ERR4035126.16".to_string()];
+        let query_ids = vec![0, 1, 2, 3, 4];
+        let block_flags = BlockFlags { queries: queries.clone(), query_ids };
+        let block_header = BlockHeader { num_records: 0, deflated_len: 0, block_len: 0, flags_len: 0, start_idx: 0, placeholder2: 0, placeholder3: 0 };
+        let (header, flags) = build_header_and_flags(&targets, &queries, &"ERR4035126".to_string()).unwrap();
+
+        let mut tmp = data.iter();
+        let mut bdecoder = BitmapDecoder::new(&mut tmp, header, flags, block_header, block_flags);
+
+        let mut got: Vec<PseudoAln> = Vec::with_capacity(expected.len());
+        for record in bdecoder.by_ref() {
+            got.push(record);
+        }
+        got.sort_by_key(|x| *x.query_id.as_ref().unwrap());
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn next_ends_with_zero() {
+        use super::BitmapDecoder;
+        use crate::PseudoAln;
+        use crate::headers::file::build_header_and_flags;
+        use crate::headers::block::BlockFlags;
+        use crate::headers::block::BlockHeader;
+
+        use roaring::RoaringBitmap;
+
+        let mut expected = vec![
+            PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(1), ones: Some(vec![0]), query_name: Some("ERR4035126.2".to_string()) },
+            PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(0), ones: Some(vec![0]), query_name: Some("ERR4035126.1".to_string()) },
+            PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(2), ones: Some(vec![0]), query_name: Some("ERR4035126.651903".to_string()) },
+        ];
+        expected.sort_by_key(|x| *x.query_id.as_ref().unwrap());
+
+        let mut data = RoaringBitmap::new();
+        data.insert(0);
+        data.insert(2);
+        data.insert(4);
+
+        let targets = vec!["chr.fasta".to_string(), "plasmid.fasta".to_string()];
+        let queries = vec!["ERR4035126.1".to_string(), "ERR4035126.2".to_string(), "ERR4035126.651903".to_string(), "ERR4035126.7543".to_string(), "ERR4035126.16".to_string()];
+        let query_ids = vec![0, 1, 2, 3, 4];
+        let block_flags = BlockFlags { queries: queries.clone(), query_ids };
+        let block_header = BlockHeader { num_records: 0, deflated_len: 0, block_len: 0, flags_len: 0, start_idx: 0, placeholder2: 0, placeholder3: 0 };
+        let (header, flags) = build_header_and_flags(&targets, &queries, &"ERR4035126".to_string()).unwrap();
+
+        let mut tmp = data.iter();
+        let mut bdecoder = BitmapDecoder::new(&mut tmp, header, flags, block_header, block_flags);
+
+        let mut got: Vec<PseudoAln> = Vec::with_capacity(expected.len());
+        for record in bdecoder.by_ref() {
+            got.push(record);
+        }
+        got.sort_by_key(|x| *x.query_id.as_ref().unwrap());
+
+        assert_eq!(got, expected);
+    }
+
+
+    #[test]
+    fn next_skips_middle() {
+        use super::BitmapDecoder;
+        use crate::PseudoAln;
+        use crate::headers::file::build_header_and_flags;
+        use crate::headers::block::BlockFlags;
+        use crate::headers::block::BlockHeader;
+
+        use roaring::RoaringBitmap;
+
+        let mut expected = vec![
+            PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(0), ones: Some(vec![0]), query_name: Some("ERR4035126.1".to_string()) },
+            PseudoAln{ones_names: Some(vec!["chr.fasta".to_string()]),  query_id: Some(2), ones: Some(vec![0]), query_name: Some("ERR4035126.651903".to_string()) },
+        ];
+        expected.sort_by_key(|x| *x.query_id.as_ref().unwrap());
+
+        let mut data = RoaringBitmap::new();
+        data.insert(0);
+        data.insert(4);
 
         let targets = vec!["chr.fasta".to_string(), "plasmid.fasta".to_string()];
         let queries = vec!["ERR4035126.1".to_string(), "ERR4035126.2".to_string(), "ERR4035126.651903".to_string(), "ERR4035126.7543".to_string(), "ERR4035126.16".to_string()];
