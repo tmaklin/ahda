2e938c87b1ef52dd3181310be2d16e6dfe31708a
tmaklin <tommi@maklin.fi>
2025-12-11 16:51:16 +0100

    Work on C++ API documentation.

--- src/cxx_api/mod.rs
+++ src/cxx_api/mod.rs
@@ -12,7 +12,26 @@
 // at your option.
 //
 
-//! C++ bindings
+//! C++ API for encoding and decoding .ahda records.
+//!
+//! The API operates on the set bit indexes of a *flattened pseudoalignment*,
+//! which is a `num_queries * num_targets` vector of boolean values, where the
+//! value (query_index, target_index) implies an alignment for the query
+//! sequence with `query_index` against the target sequence with `target_index`.
+//!
+//! The flattened pseudoalignment is assumed to be stored in a query-major
+//! format, meaning that the pseudoalignments bits of a single query sequence
+//! are stored contiguously in memory.
+//!
+//! Currently, the API only supports a 32-bit address space, meaning that the
+//! supported size of the input alignment is `num_queries * num_targets < 2^32`.
+//!
+//! **TODO** Support a larger address space.
+//!
+//! ## Usage
+//!
+//! **TODO** Usage examples for the C++ API.
+//!
 
 use crate::decode_from_read_to_roaring;
 use crate::headers::file::encode_header_and_flags;
@@ -69,6 +88,13 @@ mod ffi {
 }
 }
 
+/// Encode the file header and file flags bytes.
+///
+/// Calls [build_header_and_flags] on the input data and then creates the
+/// encoded data by calling [encode_header_and_flags].
+///
+/// The output bytes should always be written at the start of the .ahda record.
+///
 pub fn encode_file_header_and_flags(
     targets: &CxxVector<CxxString>,
     queries: &CxxVector<CxxString>,
@@ -84,6 +110,14 @@ pub fn encode_file_header_and_flags(
     bytes
 }
 
+/// Encode a single .ahda block and its block header and flags.
+///
+/// Creates a [RoaringBitmap] from the set bit indexes and calls [pack_block] to
+/// encode the block header, block flags, and block contents.
+///
+/// The output is a valid block record that can be appended to an .ahda record
+/// containing the corresponding file header and flags.
+///
 pub fn encode_block(
     queries: &CxxVector<CxxString>,
     query_ids: &CxxVector<u32>,
@@ -95,6 +129,15 @@ pub fn encode_block(
     block.unwrap()
 }
 
+/// Encode a complete .ahda record from the set bits in a flattened pseudoalignment.
+///
+/// Creates an iterator over the set bit indexes and uses a [BitmapEncoder] to
+/// encode a valid .ahda record.
+///
+/// The output is a complete .ahda record that contains the file header, file
+/// flags, and all block data required to store the alignment. This can be
+/// written to a file without further API calls.
+///
 pub fn encode_bitmap(
     targets: &CxxVector<CxxString>,
     queries: &CxxVector<CxxString>,
@@ -116,6 +159,16 @@ pub fn encode_bitmap(
     bytes
 }
 
+/// Decodes the indexes of set bits in a flattened pseudoalignment from an .ahda record.
+///
+/// Calls [decode_from_read_to_roaring] to extract a [RoaringBitmap]
+/// representing the full pseudoalignment record contained in the .ahda record.
+///
+/// The input should contain the full contents of the .ahda record to be decoded.
+///
+/// The output is a vector containing the indexes of set bits, ie.
+/// pseudoalignments, in a flattened pseudoalignment.
+///
 pub fn decode_bitmap(
     bytes: &CxxVector<u8>,
 ) -> Vec<u32> {
@@ -125,6 +178,15 @@ pub fn decode_bitmap(
     set_bits
 }
 
+/// Decodes the target sequence names from the file flags of an .ahda record.
+///
+/// The input should contain at least the bytes representing the [FileHeader]
+/// and [FileFlags] in the .ahda record.
+///
+/// The output is a vector with the names of the target sequences. The position
+/// of each element in the vector corresponds to its index in the
+/// pseudoalignment.
+///
 pub fn decode_target_names(
     bytes: &CxxVector<u8>,
 ) -> Vec<String> {
@@ -134,6 +196,15 @@ pub fn decode_target_names(
     flags.target_names
 }
 
+/// Decodes the query sequence names from the block flags in an .ahda record.
+///
+/// The input should contain a complete .ahda record with the file header and
+/// flags and all block data.
+///
+/// The output is a vector with the names of the query sequences. The position
+/// of each element in the vector does **not** necessarily correspond to its
+/// index in the original data, use [decode_query_ids] to get this information.
+///
 pub fn decode_query_names(
     bytes: &CxxVector<u8>,
 ) -> Vec<String> {
@@ -151,6 +222,16 @@ pub fn decode_query_names(
     todo!("decoding block header and block flags")
 }
 
+/// Decodes the query sequence indexes from the block flags in an .ahda record.
+///
+/// The input should contain a complete .ahda record with the file header and
+/// flags and all block data.
+///
+/// The output is a vector with the indexes of the query sequences in the
+/// original query sequence file. Use [decode_query_names] to get the vector
+/// containing the names that can be indexed by the output from
+/// decode_query_ids.
+///
 pub fn decode_query_ids(
     bytes: &CxxVector<u8>,
 ) -> Vec<u32> {
