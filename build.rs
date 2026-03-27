fb04dddaa867fe6a49a30346d7744174b3fc6df9
tmaklin <tommi@maklin.fi>
2026-03-27 13:05:47 +0100

    Add build script for CXX API

---
+++ build.rs
@@ -0,0 +1,19 @@
+// ahda: Pseudoalignment compression and conversion between formats.
+//
+// Copyright 2025 Tommi Mäklin [tommi@maklin.fi].
+//
+// Copyrights in this project are retained by contributors. No copyright assignment
+// is required to contribute to this project.
+//
+// Except as otherwise noted (below and/or in individual files), this
+// project is licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE> or <http://www.apache.org/licenses/LICENSE-2.0> or
+// the MIT license, <LICENSE-MIT> or <http://opensource.org/licenses/MIT>,
+// at your option.
+//
+
+fn main() {
+    #[cfg(feature = "cxxbridge")]
+    cxx_build::bridge("src/cxx_api/mod.rs")
+        .compile("ahda-cxx");
+}
