a4229653b2b5b12b907eda67ba51be1fcd0443f8
tmaklin <tommi@maklin.fi>
2026-08-06 13:23:08 +0200

    No unwraps without checking

--- src/decoder/bitmap_decoder.rs
+++ src/decoder/bitmap_decoder.rs
@@ -46,9 +46,9 @@ impl<I: Iterator> Iterator for BitmapDecoder<'_, I> where I: Iterator<Item=u64>{
         let mut query_id: Option<u32> = None;
 
         let n_targets: u64 = self.file_header.n_targets as u64;
-        if self.index.is_some() {
-            let query_idx = self.index.as_ref().unwrap() / n_targets;
-            let target_idx = self.index.as_ref().unwrap() % n_targets;
+        if let Some(set_bit_idx) = self.index {
+            let query_idx = set_bit_idx / n_targets;
+            let target_idx = set_bit_idx % n_targets;
             ones.push(target_idx as u32);
             query_id = Some(query_idx as u32);
             self.index = None;
@@ -56,11 +56,11 @@ impl<I: Iterator> Iterator for BitmapDecoder<'_, I> where I: Iterator<Item=u64>{
 
         for idx in self.bits_iter.by_ref() {
             self.index = Some(idx);
-            let query_idx = self.index.as_ref().unwrap() / n_targets;
+            let query_idx = idx / n_targets;
             if query_id.is_some() && query_idx as u32 != *query_id.as_ref().unwrap() {
                 break;
             }
-            let target_idx = self.index.as_ref().unwrap() % n_targets;
+            let target_idx = idx % n_targets;
             self.index = None;
             ones.push(target_idx as u32);
             query_id = Some(query_idx as u32);
